ce4c7420482409c4b87513aae459ee7ab98f8732
jason <89243713+jason330@users.noreply.github.com>
2022-10-30 18:35:55 -0700

    Complete backend phase 3

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,10 +1,19 @@
 class Api::SessionsController < ApplicationController
+
+  def show
+    if current_user
+      render json: { user: current_user }
+    else
+      render json: { user: nil }
+    end
+  end
+
   def create
     @user = User.find_by_credentials(params[:email], params[:password])
 
     if @user
       login!(@user)
-      render json: @user
+      render 'api/users/show'
     else
       render json: { errors: ['The provided credentials were invalid.'], status: :unauthorized }
     end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :show]
-    resource :session, only: [:create, :destroy]
+    resource :session, only: [:show, :create, :destroy]
   end
 
 end
