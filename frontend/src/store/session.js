7e268ec72d183f02bc4043743546a7fffdd39fed
jason <89243713+jason330@users.noreply.github.com>
2022-10-30 23:57:25 -0700

    Complete session actions and reducer

---
+++ frontend/src/store/session.js
@@ -0,0 +1,43 @@
+import csrfFetch from "./csrf";
+
+const SET_SESSION_USER = 'session/setSessionUser';
+const REMOVE_SESSION_USER = 'session/removeSessionUser';
+
+const setSessionUser = (user) => {
+    return {
+        type: SET_SESSION_USER,
+        user
+    };
+};
+
+const removeSessionUser = () => {
+    return {
+        type: REMOVE_SESSION_USER
+    };
+};
+
+const sessionReducer = (state = { user: null }, action) => {
+    switch (action.type) {
+        case SET_SESSION_USER:
+            return { ...state, user: action.user };
+        case REMOVE_SESSION_USER:
+            return { ...state, user: null };
+        default:
+            return state;
+    }
+};
+
+export const loginUser = ({email, password}) => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({
+            email,
+            password
+        })
+    });
+    const data = await response.json();
+    dispatch(setSessionUser(data.user));
+    return response;
+};
+
+export default sessionReducer;
