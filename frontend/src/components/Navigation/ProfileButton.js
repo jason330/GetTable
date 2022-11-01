c3a4e9df51c82db4ec78c09efcc48f0d52ec5532
AA Student <89243713+jason330@users.noreply.github.com>
2022-10-31 18:17:43 -0700

    Start profile dropdown

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,8 +1,18 @@
+import { useState } from 'react'
 import userIcon from './userLogo.svg'
 
 function ProfileButton() {
+    const [showMenu, setShowMenu] = useState(false)
+
     return (
-        <img className="profileButton" src={userIcon} alt="user profile button" />
+        <>
+            <button onClick={ () => setShowMenu(showMenu ? false : true) }>
+                <img className="profileButton" src={userIcon} alt="user profile button" />
+            </button>
+            <div>
+                
+            </div>
+        </>
     )
 }
 
