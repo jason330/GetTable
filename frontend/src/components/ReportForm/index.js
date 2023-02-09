062304c71333e6e24d7ff259d70673b182e72da8
jason <89243713+jason330@users.noreply.github.com>
2023-02-08 21:57:46 -0800

    Create report stars component

--- frontend/src/components/ReportForm/index.js
+++ frontend/src/components/ReportForm/index.js
@@ -1,4 +1,51 @@
+import { useState } from "react"
+
 export default function ReportForm( {user, report, restaurant, reservation} ) {
+
+    const ReportStars = () => {
+        const ratings = [1,2,3,4,5]
+        const [hoveredStar, setHoveredStar] = useState()
+        const [selectedStar, setSelectedStar] = useState()
+
+        let cssClass
+        if (hoveredStar) {
+            cssClass = hoveredStar.toString()
+        } else if (selectedStar) {
+            cssClass = selectedStar.toString()
+        } else {
+            cssClass = "ratingStar"
+        }
+
+        const ratingText = {
+            1: "Outstanding",
+            2: "Very good",
+            3: "Good",
+            4: "Fair",
+            5: "Poor"
+        }
+
+        return(
+            <>
+                <div>{ratings.map(rating =>
+                    <img
+                        src=""
+                        onMouseEnter={ () => setHoveredStar(rating) }
+                        onMouseLeave={ () => setHoveredStar() }
+                        onClick={ () => setSelectedStar(rating) }
+                        className={cssClass}
+                        key={rating}
+                        alt={`${rating}`} />
+                    )
+                }
+                </div>
+                <h4>{ratingText[selectedStar]}</h4>
+            </>
+        )
+        for (let i of numStars) {
+            <img src="" alt="" />
+
+        }
+    }
     return(
         <main>
             <h1>
@@ -6,7 +53,11 @@ export default function ReportForm( {user, report, restaurant, reservation} ) {
             </h1>
             <h2>Rate your dining experience</h2>
             <h2>Reservation made on {reservation.reservationDate}</h2>
-            <h3></h3>
+            <form className="">
+                <h3>Overall</h3>
+                <ReportStars />
+
+            </form>
         </main>
     )
 }
