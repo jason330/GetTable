da0f31052f3d8adf9732edc51a198cc1fabf71aa
jason <89243713+jason330@users.noreply.github.com>
2023-02-13 22:53:04 -0800

    Complete report rating styling

--- frontend/src/components/ReportForm/index.js
+++ frontend/src/components/ReportForm/index.js
@@ -46,15 +46,22 @@ export default function ReportForm( {user, report, restaurant, reservation, setS
 
         return(
             <>
-                <div>{ratings.map(rating =>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                <div className="starContainer">{ratings.map(rating =>
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                    className="ratingStar"
+                    width='32' height='32'
+                    viewBox="0 0 16 16"
                     onMouseEnter={ () => setHoveredStar(rating) }
                     onMouseLeave={ () => setHoveredStar() }
                     onClick={ () => setSelectedStar(rating) }
                     key={rating}
                     alt={`${rating} out of 5 stars`}
-                    ><path d="M3.213 15.984c-.063 0-.104-.01-.125-.031-.104-.063-.136-.136-.094-.219l.876-5.516L.083 6.331C0 6.247-.021 6.164.021 6.08c.021-.063.083-.115.188-.157l5.256-.783L7.781.125C7.864.042 7.937 0 8 0c.104 0 .177.042.219.125l2.347 5.015 5.226.784a.282.282 0 0 1 .188.157c.041.083.02.166-.063.25l-3.786 3.886.907 5.516a.297.297 0 0 1-.094.219c-.104.063-.188.063-.25 0L8 13.383l-4.662 2.57c-.021.021-.063.031-.125.031z"
-                    fill={numRedStars >= rating ? '#da3743' : "#E1E1E1"} /></svg>
+                    ><path 
+                    // className="starSvgPath"
+                    d="M3.213 15.984c-.063 0-.104-.01-.125-.031-.104-.063-.136-.136-.094-.219l.876-5.516L.083 6.331C0 6.247-.021 6.164.021 6.08c.021-.063.083-.115.188-.157l5.256-.783L7.781.125C7.864.042 7.937 0 8 0c.104 0 .177.042.219.125l2.347 5.015 5.226.784a.282.282 0 0 1 .188.157c.041.083.02.166-.063.25l-3.786 3.886.907 5.516a.297.297 0 0 1-.094.219c-.104.063-.188.063-.25 0L8 13.383l-4.662 2.57c-.021.021-.063.031-.125.031z"
+                    fill={numRedStars >= rating ? '#da3743' : "#E1E1E1"} />
+                    </svg>
+
                     // <img
                     //     src={star}
                     //     onMouseEnter={ () => setHoveredStar(rating) }
@@ -66,7 +73,7 @@ export default function ReportForm( {user, report, restaurant, reservation, setS
                     )
                 }
                 </div>
-                <h4>{ratingText[numRedStars]}</h4>
+                <h4 className="reportRatingText">{ratingText[numRedStars]}</h4>
             </>
         )
         // for (let i of numStars) {
@@ -107,50 +114,53 @@ export default function ReportForm( {user, report, restaurant, reservation, setS
     })
 
     return(
-        <main>
+        <div>
             <h1 className="reportHeader">
             {user.username ? user.username : user.email}, how was your experience at {restaurant.name}
             </h1>
             <h2 className="reportInstruct">Rate your dining experience</h2>
             <h2 className="reportInstruct">Reservation on {reservation.reservationDate}</h2>
             <form className="reportForm" onSubmit={handleSubmit}>
-                <h3>Overall</h3>
-                <ReportStars 
-                    hovered={hoveredOverall}
-                    selected={selectedOverall}
-                    setHovered={setHoveredOverall}
-                    setSelected={setSelectedOverall}
-                />
-                <h3>Food</h3>
-                <ReportStars 
-                    hovered={hoveredFood}
-                    selected={selectedFood}
-                    setHovered={setHoveredFood}
-                    setSelected={setSelectedFood}
-                />
-                <h3>Service</h3>
-                <ReportStars 
-                    hovered={hoveredService}
-                    selected={selectedService}
-                    setHovered={setHoveredService}
-                    setSelected={setSelectedService}
-                />
-                <h3>Ambience</h3>
-                <ReportStars 
-                    hovered={hoveredAmbience}
-                    selected={selectedAmbience}
-                    setHovered={setHoveredAmbience}
-                    setSelected={setSelectedAmbience}
-                />
-                <h3>Write a review</h3>
+                <div className="reportRatingContainer">
+                    <h3 className="reportRatingCategory">Overall</h3>
+                    <ReportStars 
+                        hovered={hoveredOverall}
+                        selected={selectedOverall}
+                        setHovered={setHoveredOverall}
+                        setSelected={setSelectedOverall}
+                    />
+                    <h3 className="reportRatingCategory">Food</h3>
+                    <ReportStars 
+                        hovered={hoveredFood}
+                        selected={selectedFood}
+                        setHovered={setHoveredFood}
+                        setSelected={setSelectedFood}
+                    />
+                    <h3 className="reportRatingCategory">Service</h3>
+                    <ReportStars 
+                        hovered={hoveredService}
+                        selected={selectedService}
+                        setHovered={setHoveredService}
+                        setSelected={setSelectedService}
+                    />
+                    <h3 className="reportRatingCategory">Ambience</h3>
+                    <ReportStars 
+                        hovered={hoveredAmbience}
+                        selected={selectedAmbience}
+                        setHovered={setHoveredAmbience}
+                        setSelected={setSelectedAmbience}
+                    />
+                </div>
+                <h3 className="reviewInstruct">Write a review</h3>
                 <h4>Help diners decide where to eat. Remember to keep it short, simple and specific.</h4>
                 <textarea name="review" id="" cols="30" rows="10"
                     onChange={e => setReview(e.target.value) }></textarea>
                 <ul>
                 {errors.map(error => <li key={error}>{error}</li>)}
                 </ul>
-                <button className="" type="submit">Submit</button>
+                <button className="red" type="submit">Submit</button>
             </form>
-        </main>
+
+        </div>
     )
 }
