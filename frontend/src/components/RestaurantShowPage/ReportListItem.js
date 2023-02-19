55e5033fbcd0bc9e6eaab36dbb581654ab61bc36
jason <89243713+jason330@users.noreply.github.com>
2023-02-18 22:33:44 -0800

    Update restaurant show page for reports without ratings

--- frontend/src/components/RestaurantShowPage/ReportListItem.js
+++ frontend/src/components/RestaurantShowPage/ReportListItem.js
@@ -10,13 +10,13 @@ export default function ReportListItem( {report} ) {
                 </div>
                 <div className="reportRatings">
                     <div className="ratingCategory">Overall</div>
-                    <div className="userRating">{report.ratingOverall}</div>
+                    <div className="userRating">{report.ratingOverall ? report.ratingOverall : 'Not rated'}</div>
                     <div className="ratingCategory">Food</div>
-                    <div className="userRating">{report.ratingFood}</div>
+                    <div className="userRating">{report.ratingFood ? report.ratingFood : 'Not rated'}</div>
                     <div className="ratingCategory">Service</div>
-                    <div className="userRating">{report.ratingService}</div>
+                    <div className="userRating">{report.ratingService ? report.ratingService : 'Not rated'}</div>
                     <div className="ratingCategory">Ambience</div>
-                    <div className="userRating">{report.ratingAmbience}</div>
+                    <div className="userRating">{report.ratingAmbience ? report.ratingAmbience : 'Not rated'}</div>
                 </div>
                 <div className="reportReview">
                     {report.review}
