a2d51307a6024e3d87ddd0ee6b3dc977bf2cf7b9
jason <89243713+jason330@users.noreply.github.com>
2023-03-17 22:19:08 -0700

    Complete search input styling

--- frontend/src/components/RestaurantIndexPage/index.js
+++ frontend/src/components/RestaurantIndexPage/index.js
@@ -44,14 +44,18 @@ function RestaurantIndexPage() {
                         role='search' 
                         onSubmit={handleSubmit}
                     >
-                        <input
-                            type="search"
-                            required
-                            className='search'
-                            placeholder="Location, Restaurant, or Cuisine"
-                            value={query}
-                            onChange={e => setQuery(e.target.value)}
-                            />
+                        <div className='search'>
+                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" focusable="false"><g fill="none" fill-rule="evenodd"><path d="M13,15.9291111 L13,21.5 C13,21.7761424 12.7761424,22 12.5,22 L11.5,22 C11.2238576,22 11,21.7761424 11,21.5 L11,15.9291111 C7.60770586,15.4438815 5,12.5264719 5,9 C5,5.13400675 8.13400675,2 12,2 C15.8659932,2 19,5.13400675 19,9 C19,12.5264719 16.3922941,15.4438815 13,15.9291111 Z M12,4 C9.23857625,4 7,6.23857625 7,9 C7,11.7614237 9.23857625,14 12,14 C14.7614237,14 17,11.7614237 17,9 C17,6.23857625 14.7614237,4 12,4 Z" fill="#2D333F" fill-rule="nonzero" transform="translate(12.000000, 12.000000) rotate(-45.000000) translate(-12.000000, -12.000000)"></path></g>
+                            </svg>
+                            <input
+                                type="text"
+                                required
+                                className=''
+                                placeholder="Location, Restaurant, or Cuisine"
+                                value={query}
+                                onChange={e => setQuery(e.target.value)}
+                                />
+                        </div>
                         <button type="submit">Let's go</button>
                     </form>
                 </div>
