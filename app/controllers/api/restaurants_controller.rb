cf361c515f8c70945d520ff2db68f3e59d566e41
jason <89243713+jason330@users.noreply.github.com>
2023-09-09 11:02:20 -0700

    Delete comments and improve readability

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -1,20 +1,19 @@
 class Api::RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
+    
     render :index
   end
 
   def show
     @restaurant = Restaurant.includes(:reports, :reservations)
       .find(params[:id])
+
     render :show
   end
 
   def search
-    # puts 'params is ',params
-    
     @query = "%" + Restaurant.sanitize_sql_like( params[:q] ) + "%"
-    # puts 'query is ',query
 
     @restaurants = Restaurant.where(
       "lower(name) LIKE ? OR lower(location) LIKE ? OR lower(cuisine) LIKE ?",
@@ -22,8 +21,7 @@ class Api::RestaurantsController < ApplicationController
       @query.downcase,
       @query.downcase
       )
-    # puts '@restaurants is', @restaurants
-    # puts '@query. class is ',@query.class
+
     render :search
   end
   
--- app/views/api/reservations/show.json.jbuilder
+++ app/views/api/reservations/show.json.jbuilder
@@ -1,8 +1,23 @@
 json.reservation do
-    json.extract! @reservation, :id, :user_id, :restaurant_id, :reservation_date, :reservation_time, :party_size
+    json.extract! @reservation,
+        :id,
+        :user_id,
+        :restaurant_id,
+        :reservation_date,
+        :reservation_time,
+        :party_size
 end
 
 json.restaurant do
-    json.extract! @reservation.restaurant, :id, :name, :description, :location, :cuisine, :opening_hour, :closing_hour, :reservation_interval
+    json.extract! @reservation.restaurant,
+        :id,
+        :name,
+        :description,
+        :location,
+        :cuisine,
+        :opening_hour,
+        :closing_hour,
+        :reservation_interval
+        
     json.photoUrl @reservation.restaurant.photos[0].url
 end
